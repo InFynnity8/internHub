7cb9e56106cd5905944fc8c82392a34e50957dab
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2023-10-27 11:25:59 +0000

    commited new

--- home/upgrade/upgrade.js
+++ home/upgrade/upgrade.js
@@ -1,6 +1,6 @@
 page1 = document.querySelector('.packageupgrage1')
 page2 = document.querySelector('.packageupgrage2')
-page3 = document.querySelector('.packageupgrage3')
+page3 = document.querySelector('.packageupgrade3')
 continueButton1 = document.querySelector('.continuebtn1')
 continueButton2 = document.querySelector('.continuebtn2')
 backButton = document.querySelector('.backbtn')
@@ -8,6 +8,11 @@ doneButton = document.querySelector('.donebtn')
 bubble1 = document.querySelector('.bubble1')
 bubble2 = document.querySelector('.bubble2')
 bubble3 = document.querySelector('.bubble3')
+debitCard = document.querySelector('.card')
+mobileMoney = document.querySelector('.momo')
+debitCard2 = document.querySelector('.card2')
+mobileMoney2 = document.querySelector('.momo2')
+paymentSwitch = document.querySelector('.payments')
 
 continueButton1.addEventListener('click', ()=>{
     page1.classList.add('action');
@@ -21,4 +26,25 @@ backButton.addEventListener('click', ()=>{
     page2.classList.remove('action');
     bubble1.classList.remove('active');
     bubble2.classList.remove('active');
+    paymentSwitch.classList.remove('active');
+});
+
+continueButton2.addEventListener('click', ()=>{
+    bubble2.classList.remove('active');
+    bubble3.classList.add('active');
+    page2.classList.add('action2');
+    page3.classList.add('action');
+    paymentSwitch.classList.remove('active');
+});
+
+mobileMoney.addEventListener('click', ()=>{
+    paymentSwitch.classList.add('active');
+});
+
+debitCard2.addEventListener('click', ()=>{
+    paymentSwitch.classList.remove('active');
+});
+
+doneButton.addEventListener('click', ()=>{
+    
 });
