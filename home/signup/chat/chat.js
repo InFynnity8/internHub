cd08aec3923fe73f95a240b1974cc2950f283082
Fynn <sackeyemmanuelfynn@gmail.com>
2023-11-09 10:55:01 -0800

    i did a commitment

--- home/signup/chat/chat.js
+++ home/signup/chat/chat.js
@@ -1,11 +1,20 @@
 const chatInput = document.querySelector(".textarea");
+const chatInput2 = document.querySelector(".textarea2");
 const sendChatBtn = document.querySelector(".sendbtn");
+const sendChatBtn2 = document.querySelector(".sendbtn2");
 const chatbox = document.querySelector(".chatbox");
+const chatbox2 = document.querySelector(".chatbox2");
 
 const inputInitHeight = chatInput.scrollHeight;
-
+const inputInitHeight2 = chatInput2.scrollHeight;
 
 document.querySelector('.textarea').addEventListener('input', function(){
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+
+})
+
+document.querySelector('.textarea2').addEventListener('input', function(){
     this.style.height = 'auto';
     this.style.height = (this.scrollHeight) + 'px';
 
@@ -45,6 +54,20 @@ const handleChat = () => {
     //append user message to the box
     chatbox.appendChild(createChatLi(userMessage, "outgoing"));
     chatbox.scrollTo(0, chatbox.scrollHeight);
+   
+}
+
+const handleChat2 = () => {
+    userMessage = chatInput2.value.trim();
+    console.log(userMessage);
+    if(!userMessage) return;
+    chatInput2.value = "";
+    chatInput2.style.height = `${inputInitHeight2}px`;
+
+    //append user message to the box
+    chatbox2.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox2.scrollTo(0, chatbox2.scrollHeight);
+   
 }
 
 chatInput.addEventListener("keydown", (e)=>{
@@ -56,4 +79,15 @@ chatInput.addEventListener("keydown", (e)=>{
     }
 });
 
+chatInput2.addEventListener("keydown", (e)=>{
+    //if enter key is pressed without shift key and the window
+    //width is greater than 800px, handle the chat
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800){
+        e.preventDefault();
+        handleChat2();
+    }
+});
+
+
 sendChatBtn.addEventListener("click", handleChat);
+sendChatBtn2.addEventListener("click", handleChat2);
