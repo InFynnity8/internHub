5725388dcca8b8d1f790a9f783d4730e2c9805ed
Fynn <sackeyemmanuelfynn@gmail.com>
2023-11-17 16:33:53 -0800

    ana section

--- home/signup/profile/analysis/analysis.js
+++ home/signup/profile/analysis/analysis.js
@@ -69,3 +69,64 @@ function updateLegend() {
 
 updateLegend();
 
+
+
+// graph
+
+
+var ctx = document.getElementById('myGraph').getContext('2d');
+
+        
+var data = {
+    labels: ['2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', ''],
+    datasets: [{
+        label: 'My Data',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        data: [31, 33, 40, 31, 26, 30, 43, 31],
+        fill: {
+            target: 'origin',
+            above: getGradient(ctx)
+        },  
+        tension: 0.4 
+   
+        
+    }]
+};
+
+
+var options = {
+    scales: {
+        y: {
+            beginAtZero: true,
+            ticks: {
+              
+                stepSize: 10, 
+                min: 0,      
+                max: 100,
+            }            
+        },
+      
+    },
+    plugins: {
+        legend: {
+            display: false 
+        }
+    }
+    
+};
+
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options
+    
+});
+
+function getGradient(context) {
+    var gradient = context.createLinearGradient(0, 0, 0, 600);
+    gradient.addColorStop(0, 'rgba(0, 100, 200, 0.5)');
+    gradient.addColorStop(1, 'rgba(228, 0, 111, 0.7)');
+    return gradient;
+}
